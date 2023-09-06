a9d40b872ee965ca564a7b5bed801290ff586dd3
YuriCdls <yuri.cdls05@gmail.com>
2023-09-05 21:35:22 -0300

    add main method

---
+++ readme.md
@@ -0,0 +1,10 @@
+
+# Título do Projeto
+
+Uma breve descrição sobre o que esse projeto faz e para quem ele é
+
+
+## Aprendizados
+
+O que você aprendeu construindo esse projeto? Quais desafios você enfrentou e como você superou-os?
+
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-
+import entidades.Produto;
 
 public class Main {
     public static void main(String[] args) {
-
+        Produto p1 = new Produto("Treloso", 3.5);
+        p1.setPreco(0.5);
+        System.out.println(p1.getPreco());
+        Produto p2 = new Produto(4567, "Banana", 7.0);
+        p2.setPreco(10);
+        
     }
 }
